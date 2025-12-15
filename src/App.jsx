b520601192dd486e2a23423a17173259b5701b5c
sirpahaataja <sirpahaataja19@gmail.com>
2025-12-15 11:22:57 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import { useState } from 'react'
+import './App.css'
+import { v4 as uuidv4 } from 'uuid'
+
+function App() {
+  const [form, setForm] = useState({aihe:"", paiva:"", muistiinpano:"" });
+  const [muistiinpanotListassa, setMuistiinpanotListassa] = useState([]);
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //console.log("formilta: ", form);
+
+    const muistiinpano = {};
+    muistiinpano.id = uuidv4(); 
+    muistiinpano.aihe=form.aihe;
+    muistiinpano.paiva=form.paiva;
+    muistiinpano.muistiinpano=form.muistiinpano;
+    
+    const uusiLista = muistiinpanotListassa.concat(muistiinpano);
+
+    //console.log("uusi: ",uusiLista);
+
+    setMuistiinpanotListassa(uusiLista);
+
+    setForm({aihe:"", paiva:"", muistiinpano:"" });
+
+    //console.log("listassa: ", muistiinpanotListassa);
+  }
+  const poista= (id) => {
+    // uuid
+    //console.log("id poistoon: ", id);
+    const uusiLista = muistiinpanotListassa.filter((item) => item.id !== id);
+    setMuistiinpanotListassa(uusiLista);
+
+    //console.log(muistiinpanotListassa);
+  }
+
+  return (
+    <>
+      <h1>Osaamispäiväkirja 15.12.2025</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Aihe: 
+          <input type="text" id="aihe" name="aihe" required
+          value={form.aihe}
+          onChange={(e) => setForm({...form, [e.target.name]: e.target.value})}
+          />
+        </label>
+         <label>Päivämäärä: 
+          <input type="date" id="paiva" name="paiva" required
+          value={form.paiva}
+          onChange={(e) => setForm({...form, [e.target.name]: e.target.value})}
+          />
+        </label>
+        <label>Muistiinpano: 
+          <textarea id="muistiinpano" name="muistiinpano" required
+          value={form.muistiinpano}
+          onChange={(e) => setForm({...form, [e.target.name]: e.target.value})}
+          />
+        </label>
+        <button>Lisää uusi muistiinpano</button>
+      </form>
+      
+      <h2>Muistiinpanot:</h2>
+          <ul>
+            {muistiinpanotListassa.map((item) => (
+            <li key={item.id}>
+              {item.aihe}
+              {item.paiva}
+              {item.muistiinpano}
+              <button type="button"                
+                onClick={() => poista(item.id)}
+                >Poista
+              </button>
+            </li>
+          ))} 
+          </ul>
+    </>
+  )
+}
+
+export default App
